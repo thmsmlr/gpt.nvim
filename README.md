31c92f19d5c6f66627f9646eb25d39d04b0219f0
Thomas Millar <millar.thomas@gmail.com>
2023-03-06 09:57:11 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# gpt.nvim
+
+Twitter's favorite LLM in your VIM.
+
+Installation is easy. 
+With your favorite package manager,
+
+```
+{
+  "thmsmlr/gpt.nvim",
+  config = function()
+    require('gpt').setup({
+      api_key = os.getenv("OPENAI_API_KEY")
+    })
+
+    opts = { silent = true, noremap = true }
+    vim.keymap.set('v', '<C-g>r', require('gpt').replace, opts)
+    vim.keymap.set('v', '<C-g>p', require('gpt').visual_prompt, opts)
+    vim.keymap.set('i', '<C-g>p', require('gpt').prompt, opts)
+  end
+}
+```
+
+You can get an API key via the [OpenAI user settings page](https://platform.openai.com/account/api-keys)
+
+## Usage
+
+There are three ways to use the plugin:
+
+1. In Visual Mode, select text and rewrite it using `<C-g>r`.
+It'll ask you for the prompt to customize how GPT rewrites the selection.
+
+[Video1]
+
+2. In Visual Mode, select text and use it as the prompt with `<C-g>p`.
+ChatGPT will respond 2 lines below the selection.
+
+[Video2]
+
+3. In Normal Mode, `<C-g>p` will ask you for a prompt.
+ChatGPT will insert it's response into the buffer at the cursor's location.
+
+[Video3]
+
---
+++ lua/gpt.lua
@@ -0,0 +1,201 @@
+local M = {}
+
+-- Setup API key
+M.setup = function(opts)
+	local api_key = opts.api_key
+	if api_key == nil then
+		print("Please provide an OpenAI API key")
+		return
+	end
+
+	-- Make sure the share directory exists
+	local share_dir = os.getenv("HOME") .. "/.local/share/nvim"
+	if vim.fn.isdirectory(share_dir) == 0 then
+		vim.fn.mkdir(share_dir, "p")
+	end
+
+	-- Setup API key
+	vim.g.gpt_api_key = api_key
+end
+
+--[[
+Given a prompt, call chatGPT and stream back the results one chunk
+as a time as they are streamed back from OpenAI.
+
+```
+require('gpt').stream("What is the meaning of life?", {
+	trim_leading = true, -- Trim leading whitespace of the response
+	on_chunk = function(chunk)
+		print(chunk)
+	end
+})
+```
+]]
+--
+M.stream = function(prompt, opts)
+	if vim.g.gpt_api_key == nil then
+		print("Please provide an OpenAI API key require('gpt').setup({})")
+		return
+	end
+
+	local payload = {
+		stream = true,
+		model = "gpt-3.5-turbo",
+		messages = { { role = "user", content = prompt } },
+	}
+
+	local identity = function(chunk)
+		return chunk
+	end
+
+	opts = opts or {}
+	local cb = opts.on_chunk or identity
+	local trim_leading = opts.trim_leading or true
+	local encoded_payload = vim.fn.json_encode(payload)
+
+	-- Write payload to temp file
+	local params_path = os.getenv("HOME") .. "/.local/share/nvim/gpt.query.json"
+	local temp = io.open(params_path, "w")
+	if temp ~= nil then
+		temp:write(encoded_payload)
+		temp:close()
+	end
+
+	local command =
+		"curl --no-buffer https://api.openai.com/v1/chat/completions " ..
+		"-H 'Content-Type: application/json' -H 'Authorization: Bearer " .. vim.g.gpt_api_key .. "' " ..
+		"-d @" .. params_path .. " | tee ~/.local/share/nvim/gpt.log 2>/dev/null"
+
+	-- Write command to log file
+	local log = io.open(os.getenv("HOME") .. "/.local/share/nvim/gpt.log", "w")
+	if log ~= nil then
+		log:write(command)
+		log:close()
+	end
+
+	vim.fn.jobstart(command, {
+		stdout_buffered = false,
+		on_stdout = function(_, data, _)
+			for _, line in ipairs(data) do
+				if line ~= "" then
+					-- Strip token to get down to the JSON
+					line = line:gsub("^data: ", "")
+					if line == "[DONE]" then
+						break
+					end
+					local json = vim.fn.json_decode(line)
+					local chunk = json.choices[1].delta.content
+
+					if chunk ~= nil then
+						if trim_leading then
+							chunk = chunk:gsub("^%s+", "")
+							if chunk ~= "" then
+								trim_leading = false
+							end
+						end
+						cb(chunk)
+					end
+				end
+			end
+		end,
+	})
+end
+
+local function get_visual_selection()
+	vim.cmd('noau normal! "vy"')
+	vim.cmd('noau normal! gv')
+	return vim.fn.getreg('v')
+end
+
+local function send_keys(keys)
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes(keys, true, false, true),
+		'm', true
+	)
+end
+
+--[[
+In visual mode given some selected text, ask the user how they
+would like it to be rewritten. Then rewrite it that way.
+]]
+--
+M.replace = function()
+	local mode = vim.api.nvim_get_mode().mode
+	if mode ~= "v" and mode ~= "V" then
+		print("Please select some text")
+		return
+	end
+
+	local text = get_visual_selection()
+
+	local prompt = "Rewrite this text "
+	prompt = prompt .. vim.fn.input("[Prompt]: " .. prompt)
+	prompt = prompt .. ": \n\n" .. text .. "\n\nRewrite:"
+
+	send_keys("d")
+
+	if mode == 'V' then
+		send_keys("O")
+	end
+
+	M.stream(prompt, {
+		trim_leading = true,
+		on_chunk = function(chunk)
+			chunk = vim.split(chunk, "\n", {})
+			vim.api.nvim_put(chunk, "c", mode == 'V', true)
+			vim.cmd('undojoin')
+		end
+	})
+end
+
+--[[
+Ask the user for a prompt and insert the response where the cursor
+is currently positioned.
+]]
+--
+M.prompt = function()
+	local mode = vim.api.nvim_get_mode().mode
+	M.stream(vim.fn.input('Prompt: '), {
+		trim_leading = true,
+		on_chunk = function(chunk)
+			chunk = vim.split(chunk, "\n", {})
+			vim.api.nvim_put(chunk, "c", mode == 'V', true)
+			vim.cmd('undojoin')
+		end
+	})
+end
+
+--[[
+Take the current visual selection as the prompt to chatGPT.
+Insert the response one line below the current selection.
+]]
+--
+M.visual_prompt = function()
+	local mode = vim.api.nvim_get_mode().mode
+	local text = get_visual_selection()
+
+	local prompt = ""
+	prompt = prompt .. vim.fn.input("[Prompt]: " .. prompt)
+	prompt = prompt .. "\n\n ===== \n\n" .. text .. "\n\n ===== \n\n"
+
+	send_keys("<esc>")
+
+	if mode == 'V' then
+		send_keys("o")
+	end
+
+	send_keys("<CR><esc>O")
+
+	M.stream(prompt, {
+		trim_leading = true,
+		on_chunk = function(chunk)
+			chunk = vim.split(chunk, "\n", {})
+			vim.api.nvim_put(chunk, "c", mode == 'V', true)
+			vim.cmd('undojoin')
+		end
+	})
+
+	send_keys("<esc>")
+end
+
+return M
